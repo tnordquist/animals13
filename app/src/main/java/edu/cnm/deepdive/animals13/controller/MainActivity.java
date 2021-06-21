5373e6114a353c00447d6dcd2e8410a207e2cfe2
Todd Nordquist <toddnordquist144@gmail.com>
2021-06-21 17:04:44 -0600

    Replaced Thread with AsyncTask.

--- app/src/main/java/edu/cnm/deepdive/animals13/controller/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/animals13/controller/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.cnm.deepdive.animals13.controller;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -42,31 +43,40 @@ public class MainActivity extends AppCompatActivity {
 
       }
     });
-    new Retriever().start();
+    new RetrieverTask().execute();
   }
 
-  private class Retriever extends Thread {
+  private class RetrieverTask extends AsyncTask<Void, Void, List<Animal>> {
 
     @Override
-    public void run() {
+    protected List<Animal> doInBackground(Void... voids) {
       try {
         Response<List<Animal>> response = WebServiceProxy.getInstance()
             .getAnimals(BuildConfig.API_KEY)
             .execute();
         if (response.isSuccessful()) {
-          List<Animal> animals = response.body();
-            adapter = new ArrayAdapter<>(MainActivity.this,
-                R.layout.item_animal_spinner, animals);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
-          runOnUiThread(() -> {
-            animalSelector.setAdapter(adapter);
-          });
+          return response.body();
         } else {
           Log.e(getClass().getName(), response.message());
+          cancel(true);
+          return null;
         }
       } catch (IOException e) {
         Log.e(getClass().getName(), e.getMessage(), e);
+        cancel(true);
+        return null;
       }
     }
+
+    @Override
+    protected void onPostExecute(List<Animal> animals) {
+      super.onPostExecute(animals);
+      adapter = new ArrayAdapter<>(MainActivity.this,
+          R.layout.item_animal_spinner, animals);
+      adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+      animalSelector.setAdapter(adapter);
+    }
+
   }
+
 }
